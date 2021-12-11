f96d830ba10aab773bda6c366ee6cf60e374debf
mickovskaeleni <mickovska.eleni@gmail.com>
2021-12-11 20:04:33 +0100

    Change params range; Implement reactivity on params

--- R/eleni/server.R
+++ R/eleni/server.R
@@ -10,7 +10,7 @@ server <- function(input, output) {
       Parameter = c("C_MAX",
                "DDFA",
                "RETCAP",
-               "CAN_ST",
+               "CANS_ST",
                "STEM_ST",
                "CDIV",
                "SDIV",
@@ -20,7 +20,7 @@ server <- function(input, output) {
       Value = as.character(c(input$c_max,
                              input$ddfa,
                              input$retcap,
-                             input$can_st,
+                             input$cans_st,
                              input$stem_st,
                              input$cdiv,
                              input$sdiv,
@@ -36,88 +36,90 @@ server <- function(input, output) {
     sliderValues()
   })
   
+  #x1 <- reactive(input$c_max)
   ####
   # run dhrum
   
-  # START put this to the environment global variables
-  days=c(30,60,90,120,150,180,210,240,270,300,330,355,364)
-  p_OBS=days/365.25
-  # RaBP = 96# odhad Martin Hanel
-  QmBP = c(26, 18, 14, 12, 10, 8.0, 7.0, 6.0, 4.5, 3.5, 2.5, 1.0, 0.5)#l/s in 1 day
-  A=4.7*1000*1000# plocha BP
-  RmBP = QmBP * (3600*24) / A #CHMU ZHU mm/day
-  parsDF = data.table( B_SOIL = 1,
-                       C_MAX = 450,
-                       B_EVAP = 1,
-                       KS = 0.001,
-                       KF = 0.3,
-                       ADIV = 0.4,
-                       CDIV = 0.3,
-                       SDIV = 0.05,
-                       CAN_ST = 1.5,
-                       STEM_ST = .2,
-                       CSDIV = 0.08,
-                       TETR = 0,
-                       DDFA = 5,
-                       TMEL = 0.0,
-                       RETCAP = 2,
-                       CMIN =10)
-  filname2 = "~/tmp/dHRUM/dHRUM/Calibrations/Amalie/indata/KL_1960_01_01_noDate.txt"
-  TPdta = read.table(filname2)
-  prec=TPdta$V1
-  temp=TPdta$V2
-  nHrusBP <- 1
-  AreasBP <- 4.7*1000*1000
-  IdsHrus <- paste0("BP",seq(1:length(AreasBP)))
-  # end global variables
+  #v <- reactiveValues(data = NULL)
+  outDta <- reactiveValues(data = NULL)
   
-  
-  BP_run = function(pars = parsDF){
-    BPdhrus <- initdHruModel(nHrusBP,AreasBP,IdsHrus)
-    setPTInputsToAlldHrus(BPdhrus, Prec = prec, Temp = temp, inDate = as.Date("1960/01/01"))
-    calcPetToAllHrus(dHRUM_ptr = BPdhrus,50.1,"HAMON")
-    setGWtypeToAlldHrus(dHRUM_ptr = BPdhrus ,gwTypes=rep("LIN_RES", times=1),hruIds=IdsHrus)
-    setSoilStorTypeToAlldHrus(dHRUM_ptr = BPdhrus,soilTypes=rep("PDM",times= 1),hruIds=IdsHrus)
-    setParsToDistdHRUM(BPdhrus, pars, F)
-    # setParsToDistdHRUM(BPdhrus, ParBest, F)
-    dta<-dHRUMrun(dHRUM_ptr = BPdhrus)
-    dtaDF <- as.data.frame(dta$outDta)
-    names(dtaDF) <- dta$VarsNams
-    dtaDF <- as.data.table(dtaDF)
-    
-    dtaDF[,DTM:=as.Date(paste(YEAR,MONTH,DAY,sep="-"))]
-    
-    dtaDF[,YEAR:=NULL]
-    dtaDF[,MONTH:=NULL]
-    dtaDF[,DAY:=NULL]
-    dtaDF[,JDAY:=NULL]
-    
-    simBest=as.numeric(quantile(dtaDF$TOTR,probs=(1-p_OBS), na.rm = TRUE))
-    
-    return (list(FDC = simBest, dta = copy(dtaDF)))
-  }
-  
-  out = BP_run(pars = parsDF)
-  #out = BP_run(pars = ParBest)
-  
-  v <- reactiveValues(data = NULL)
-  
-  #  possible graphical output
   observeEvent(input$dhrum, {
-    v$data <- out
+    
+    # START put this to the environment global variables
+    days=c(30,60,90,120,150,180,210,240,270,300,330,355,364)
+    p_OBS=days/365.25
+    # RaBP = 96# odhad Martin Hanel
+    QmBP = c(26, 18, 14, 12, 10, 8.0, 7.0, 6.0, 4.5, 3.5, 2.5, 1.0, 0.5)#l/s in 1 day
+    A=4.7*1000*1000# plocha BP
+    RmBP = QmBP * (3600*24) / A #CHMU ZHU mm/day
+    parsDF = data.table( B_SOIL = 1,
+                         C_MAX = input$c_max,
+                         B_EVAP = 1,
+                         KS = input$ks,
+                         KF = input$kf,
+                         ADIV = input$adiv,
+                         CDIV = input$cdiv,
+                         SDIV = input$sdiv,
+                         CAN_ST = input$cans_st,
+                         STEM_ST = input$stem_st,
+                         CSDIV = 0.08,
+                         TETR = 0,
+                         DDFA = input$ddfa,
+                         TMEL = 0.0,
+                         RETCAP = input$retcap,
+                         CMIN =10)
+    filname2 = "~/tmp/dHRUM/dHRUM/Calibrations/Amalie/indata/KL_1960_01_01_noDate.txt"
+    TPdta = read.table(filname2)
+    prec=TPdta$V1
+    temp=TPdta$V2
+    nHrusBP <- 1
+    AreasBP <- 4.7*1000*1000
+    IdsHrus <- paste0("BP",seq(1:length(AreasBP)))
+    # end global variables
+    
+    
+    BP_run = function(pars = parsDF){
+      BPdhrus <- initdHruModel(nHrusBP,AreasBP,IdsHrus)
+      setPTInputsToAlldHrus(BPdhrus, Prec = prec, Temp = temp, inDate = as.Date("1960/01/01"))
+      calcPetToAllHrus(dHRUM_ptr = BPdhrus,50.1,"HAMON")
+      setGWtypeToAlldHrus(dHRUM_ptr = BPdhrus ,gwTypes=rep("LIN_RES", times=1),hruIds=IdsHrus)
+      setSoilStorTypeToAlldHrus(dHRUM_ptr = BPdhrus,soilTypes=rep("PDM",times= 1),hruIds=IdsHrus)
+      setParsToDistdHRUM(BPdhrus, pars, F)
+      # setParsToDistdHRUM(BPdhrus, ParBest, F)
+      dta<-dHRUMrun(dHRUM_ptr = BPdhrus)
+      dtaDF <- as.data.frame(dta$outDta)
+      names(dtaDF) <- dta$VarsNams
+      dtaDF <- as.data.table(dtaDF)
+      
+      dtaDF[,DTM:=as.Date(paste(YEAR,MONTH,DAY,sep="-"))]
+      
+      dtaDF[,YEAR:=NULL]
+      dtaDF[,MONTH:=NULL]
+      dtaDF[,DAY:=NULL]
+      dtaDF[,JDAY:=NULL]
+      
+      simBest=as.numeric(quantile(dtaDF$TOTR,probs=(1-p_OBS), na.rm = TRUE))
+      
+      return (list(FDC = simBest, dta = copy(dtaDF)))
+    }
+    
+    out = BP_run(pars = parsDF)
+    
+    outDta$data <- out
   })
   
+  
   output$plotFDC <- renderPlot({
-    if (is.null(v$data)) return()
-    plot = plot(days,RmBP, pch=19, ylim= range(c(out$FDC,RmBP)), ylab="Qm [mm/den]", xlab="Day")
-    plot = plot + points(days,out$FDC,col="red",pch=19)
+    if (is.null(outDta$data)) return()
+    plot = plot(days,RmBP, pch=19, ylim= range(c(outDta$data$FDC,RmBP)), ylab="Qm [mm/den]", xlab="Day")
+    plot = plot + points(days,outDta$data$FDC,col="red",pch=19)
     grid()
   })
   
   output$plotHydrograph <- renderPlot({
-    if (is.null(v$data)) return()
-    plot = plot(out$dta$DTM,out$dta$TOTR, type="l", xlab="Date", ylab="Q [mm/den]")
-    plot = plot + lines(out$dta$DTM,out$dta$BASF,col='red')
+    if (is.null(outDta$data)) return()
+    plot = plot(outDta$data$dta$DTM,outDta$data$dta$TOTR, type="l", xlab="Date", ylab="Q [mm/den]")
+    plot = plot + lines(outDta$data$dta$DTM,outDta$data$dta$BASF,col='red')
     grid()
   })
   
--- R/eleni/ui.R
+++ R/eleni/ui.R
@@ -5,12 +5,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("c_max", "C_MAX:", min = 0, max = 600, value = 450),
-      sliderInput("ddfa", "DDFA:", min = 0, max = 600, value = 5),
-      sliderInput("retcap", "RETCAP:", min = 0, max = 1000, value = 500),
-      sliderInput("can_st", "CAN_ST:", min = 0, max = 10, value = 1.5, step = 0.001),
-      sliderInput("stem_st", "STEM_ST:", min = 0, max = 10, value = 0.2, step = 0.001),
-      sliderInput("cdiv", "CDIV:", min = 0, max = 1, value = 0.3, step = 0.001),
-      sliderInput("sdiv", "SDIV:", min = 0, max = 1, value = 0.05, step = 0.001),
+      sliderInput("ddfa", "DDFA:", min = 0, max = 10, value = 5),
+      sliderInput("retcap", "RETCAP:", min = 0, max = 20, value = 2),
+      sliderInput("cans_st", "CANS_ST:", min = 0, max = 5, value = 1.5, step = 0.001),
+      sliderInput("stem_st", "STEM_ST:", min = 0, max = 2, value = 0.2, step = 0.001),
+      sliderInput("cdiv", "CDIV:", min = 0, max = 0.5, value = 0.3, step = 0.001),
+      sliderInput("sdiv", "SDIV:", min = 0, max = 0.2, value = 0.05, step = 0.001),
       sliderInput("ks", "KS:", min = 0, max = 1, value = 0.001, step = 0.0001),
       sliderInput("kf", "KF:", min = 0, max = 1, value = 0.3, step = 0.01),
       sliderInput("adiv", "ADIV:", min = 0, max = 1, value = 0.4, step = 0.01)
@@ -19,11 +19,10 @@ ui <- fluidPage(
     mainPanel(
       # Output: Table summarizing the values entered ----
       tableOutput("values"),
-      actionButton("dhrum", "Run dHRUM"),
+      actionButton("dhrum", "Run dHRUM", class = "btn-lg btn-success"),
       hr(),
       plotOutput("plotFDC"),
       plotOutput("plotHydrograph")
-      
     )
   )
 )
