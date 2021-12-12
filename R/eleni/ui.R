e8fae347a727bbab0041f2c1d9a64c19c70ae1a0
mickovskaeleni <mickovska.eleni@gmail.com>
2021-12-12 22:21:59 +0100

    Fix height on tab panel

--- R/eleni/ui.R
+++ R/eleni/ui.R
@@ -16,66 +16,48 @@ ui <- fluidPage(
                                  tabPanel(
                                    "Direct Runoff",
                                    fluid = F,
-                                   sidebarLayout(
-                                     sidebarPanel(
                                        width = 12,
                                        sliderInput("ks", "KS:", min = 0, max = 1, value = 0.001, step = 0.001),
                                        sliderInput("kf", "KF:", min = 0, max = 1, value = 0.3, step = 0.01),
-                                       sliderInput("adiv", "ADIV:", min = 0, max = 1, value = 0.4, step = 0.01)
-                                     ),
-                                     mainPanel(width = 0)
-                                   )
+                                       sliderInput("adiv", "ADIV:", min = 0, max = 1, value = 0.4, step = 0.01),
+                                       style = "height: 550px;",
                                  ),
                                  tabPanel(
                                    "Vegetation" ,
                                    fluid = F,
-                                   sidebarLayout(
-                                     sidebarPanel(
                                        width = 12,
                                        sliderInput("cans_st", "CANS_ST:", min = 0, max = 5, value = 1.5, step = 0.01),
                                        sliderInput("stem_st", "STEM_ST:", min = 0, max = 2, value = 0.2, step = 0.01),
                                        sliderInput("cdiv", "CDIV:", min = 0, max = 0.5, value = 0.3, step = 0.01),
                                        sliderInput("sdiv", "SDIV:", min = 0, max = 0.2, value = 0.05, step = 0.01),
-                                       sliderInput("csdiv", "CSDIV:", min = 0, max = 1, value = 0.08, step = 0.01)
-                                     ),
-                                     mainPanel(width = 0)
-                                   )
+                                       sliderInput("csdiv", "CSDIV:", min = 0, max = 1, value = 0.08, step = 0.01),
+                                       style = "height: 550px;"
                                  ),
                                  
                                  tabPanel(
                                    "Soil",
                                    fluid = F,
-                                   sidebarLayout(
-                                     sidebarPanel(
                                        width = 12,
                                        sliderInput("c_max", "C_MAX:", min = 0, max = 600, value = 450, step = 1),
                                        sliderInput("b_soil", "B_SOIL:", min = 0.5, max = 2.5, value = 1, step = 0.01),
                                        sliderInput("b_evap", "B_EVAP:", min = 0.5, max = 2.5, value = 1, step = 0.01),
                                        sliderInput("c_min", "C_MIN:", min = 0, max = 20, value = 10, step = 0.1),
-                                       sliderInput("retcap", "RETCAP:", min = 0, max = 20, value = 2, step = 0.1)
-                                     ),
-                                     mainPanel(width = 0)
-                                   )
+                                       sliderInput("retcap", "RETCAP:", min = 0, max = 20, value = 2, step = 0.1),
+                                       style = "height: 550px;"
                                  ),
                                  tabPanel(
                                    "Snow",
                                    fluid = F,
-                                   sidebarLayout(
-                                     sidebarPanel(
                                        width = 12,
                                        sliderInput("ddfa", "DDFA:", min = 0, max = 10, value = 5, step = 0.1),
                                        sliderInput("tetr", "TETR:", min = -0.5, max = 4.5, value = 0, step = 0.1),
-                                       sliderInput("tmel", "TMEL:", min = -3, max = 1, value = 0.0, step = 0.1)
-                                     ),
-                                     mainPanel(width = 0)
-                                   )),
+                                       sliderInput("tmel", "TMEL:", min = -3, max = 1, value = 0.0, step = 0.1),
+                                       style = "height: 550px;"
+                                  )
                                ),
+                               style = "padding-left: 10px;",
                                actionButton("runDhrum", "Run dHRUM", class = "btn-success")
                              )
-                             # fluidRow(
-                             #   tableOutput("values")
-                             #   
-                             # )
                   
                 ),
                 mainPanel(width = 8,
